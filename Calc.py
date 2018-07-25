64ee7bd9559384bc127fbe30cfed945430abb999
changhyeon.heo <changhyeon.heo@navercorp.com>
2018-07-25 18:02:14 +0900

    CloudFunction Calculator Sample

---
+++ Calc.py
@@ -0,0 +1,6 @@
+class Calc:
+
+    @staticmethod
+    def sum(num1, num2):
+        result = num1 + num2
+        return result
---
+++ __main__.py
@@ -0,0 +1,19 @@
+import Calc
+
+
+def main(args):
+
+    num1 = args.get("num1", 1)
+    num2 = args.get("num2", 2)
+
+    result = Calc.Calc.sum(num1, num2)
+
+    print("## num1 : " + str(num1))
+    print("## num2 : " + str(num2))
+    print("## result : "+str(result))
+
+    return {"payload": result}
+
+
+if __name__ == '__main__':
+    main({"num1": 2, "num2": 3})
