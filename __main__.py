6bbdf54a14f9f7b7db717695dc4be3877da22401
changhyeon.heo <changhyeon.heo@navercorp.com>
2018-07-31 11:16:39 +0900

    CloudFunction Calculator Sample

--- __main__.py
+++ __main__.py
@@ -4,31 +4,31 @@ from sms_sender import SmsSender
 
 # cloud function : calculator
 
-# def main(args):
-#
-#     num1 = args.get("num1", 1)
-#     num2 = args.get("num2", 2)
-#
-#     result = calc.Calc.sum(num1, num2)
-#
-#     print("## num1 : " + str(num1))
-#     print("## num2 : " + str(num2))
-#     print("## result : "+str(result))
-#
-#     return {"payload": result}
+def main(args):
+
+    num1 = args.get("num1", 1)
+    num2 = args.get("num2", 2)
+
+    result = calc.Calc.sum(num1, num2)
+
+    print("## num1 : " + str(num1))
+    print("## num2 : " + str(num2))
+    print("## result : "+str(result))
+
+    return {"payload": result}
 
 
 # cloud function : SENS
 
-def main(args):
-
-    res = SmsSender.req_sms()
-
-    print("response status:\n%d" % res.getcode())
-    print("response info:\n%s" % res.info())
-    print("response body:\n%s" % res.read())
-
-    return {"result": "success"}
+# def main(args):
+#
+#     res = SmsSender.req_sms()
+#
+#     print("response status:\n%d" % res.getcode())
+#     print("response info:\n%s" % res.info())
+#     print("response body:\n%s" % res.read())
+#
+#     return {"result": "success"}
 
 
 if __name__ == '__main__':
